1f6edd5d2bbbf4a1966a17c8732a9e1826d44c68
Abraham_Demsew <abrahamdemsew@gmail.com>
2026-02-06 12:06:38 +0300

    the subject route is impelemented

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
+import subjectRouter from './routes/subject';
+import cors from 'cors';
 
 const app = express();
 const PORT = Number(process.env.PORT ?? 8000);
 
 app.use(express.json());
 
+if(!process.env.FRONTEND_URL){
+	console.warn('FRONTEND_URL is not defined. CORS will not be configured properly.');
+}
+
+app.use(cors({
+	origin: process.env.FRONTEND_URL,
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true,
+}));
+
 app.get('/', (_req, res) => {
 	res.send('Classroom backend is running.');
 });
 
-
+app.use('/api/subjects', subjectRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
